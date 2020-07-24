66a9b15d03a4bf665d09774291f37cd1f9167590
임도현 <gigjf@naver.com>
2020-07-24 15:28:48 +0900

    "S03P12B207-29" thumbnail_progressing

--- frontend/src/components/article/ArticleListComp.vue
+++ frontend/src/components/article/ArticleListComp.vue
@@ -4,8 +4,14 @@
     <td>{{user_num}}</td>
     <td>{{title}}</td>
     <td>
-      <v-img :src="require(thumbnail)"/>
+      <!-- <img :src="imgSrc()"> -->
+      <!-- <img src= "../../assets/1.png"> -->
+      <!-- <v-img :src = thumbnail> -->
+      <!-- <v-img :src="require('../../assets/1.png')"/> -->
       <!-- {{thumbnail}} -->
+      <!-- <img src = "../../assets/1.png" contain height="200"> -->
+      <img src = "@/assets/1.png" contain height="200">
+      <img v-bind:src="thumbnail" contain height="200">
     </td>
   </tr>
 </template>
@@ -18,6 +24,13 @@ export default {
     user_num: { type: Number },
     title: { type: String },
     thumbnail: { type: String },
+  },
+  methods: {
+    imgSrc : function() {
+      // return this.thumbnail;
+      // return createElement('img', { attrs: { src: require(this.thumbnail) }});
+      return "@/assets/1.png";
+    },
   }
 }
 </script>
