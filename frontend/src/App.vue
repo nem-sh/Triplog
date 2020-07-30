dd0855ddddc1ecfcd076c98824fec62c6b4e509b
남시성 <joyclub2020@naver.com>
2020-07-30 13:41:44 +0900

    personal_main_board_ing_S03P12B207-71

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,200 +2,154 @@
   <v-app>
     <div>
       <v-navigation-drawer
-          :mini-variant.sync="mini"
-          clipped
-          app
-          permanent
-          :height="clientHeight"
-          v-if="this.getProfile"
-        >
-          <v-list-item class="px-2" style="padding: 10px;">
-            <v-list-item-avatar :color="ranColor" size="40">
-              <span class="white--text headline">{{avatarName(this.getProfile)}}</span>
-            </v-list-item-avatar>
+        :mini-variant.sync="mini"
+        clipped
+        app
+        permanent
+        :height="clientHeight"
+        v-if="this.getProfile"
+      >
+        <v-list-item class="px-2" style="padding: 10px;">
+          <v-list-item-avatar :color="ranColor" size="40">
+            <span class="white--text headline">{{avatarName(this.getProfile)}}</span>
+          </v-list-item-avatar>
 
-            <v-list-item-title>{{this.getProfile}}</v-list-item-title>
-          
-            <v-btn icon @click.stop="mini = !mini">
-              <v-icon>mdi-chevron-left</v-icon>
-            </v-btn>
+          <v-list-item-title>{{this.getProfile}}</v-list-item-title>
+
+          <v-btn icon @click.stop="mini = !mini">
+            <v-icon>mdi-chevron-left</v-icon>
+          </v-btn>
+        </v-list-item>
+
+        <v-divider></v-divider>
+
+        <v-list>
+          <v-list-item to="/">
+            <v-list-item-icon>
+              <v-icon>mdi-home-city</v-icon>
+            </v-list-item-icon>
+
+            <v-list-item-content>
+              <v-list-item-title>Home</v-list-item-title>
+            </v-list-item-content>
           </v-list-item>
 
-          <v-divider></v-divider>
+          <v-list-item @click.stop="info">
+            <v-list-item-icon>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-icon>
 
-          <v-list>
-            <v-list-item to="/">
-              <v-list-item-icon>
-                <v-icon>mdi-home-city</v-icon>
-              </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>My Account</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
 
-              <v-list-item-content>
-                <v-list-item-title>Home</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+          <v-list-item to="/like">
+            <v-list-item-icon>
+              <v-icon color="pink">mdi-charity</v-icon>
+            </v-list-item-icon>
 
-            <v-list-item @click.stop="info">
-              <v-list-item-icon>
-                <v-icon>mdi-account</v-icon>
-              </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>좋아요 목록</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
 
-              <v-list-item-content>
-                <v-list-item-title>My Account</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+          <v-list-item :to="{name: 'articleList', params: {hostNum: this.getUserNum}}">
+            <v-list-item-icon>
+              <v-icon>mdi-inbox-full</v-icon>
+            </v-list-item-icon>
 
-            <v-list-item to="/like">
-              <v-list-item-icon>
-                <v-icon color="pink">mdi-charity</v-icon>
-              </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title>게시물 목록</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
 
-              <v-list-item-content>
-                <v-list-item-title>좋아요 목록</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
+        <div class="pa-2" style="text-align: center;">
+          <v-btn inline-block @click.stop="logout">로그아웃</v-btn>
+        </div>
+      </v-navigation-drawer>
 
-            <v-list-item :to="{name: 'articleList', params: {hostNum: this.getUserNum}}">
-              <v-list-item-icon>
-                <v-icon>mdi-inbox-full</v-icon>
-              </v-list-item-icon>
+      <v-app-bar app dense color="#6A76AB" dark clipped-left>
+        <v-toolbar-title style="margin: 0 auto; font-size: 40px;">
+          TRIPLOG
+          <v-icon>mdi-compass-outline</v-icon>
+        </v-toolbar-title>
 
-              <v-list-item-content>
-                <v-list-item-title>게시물 목록</v-list-item-title>
-              </v-list-item-content>
-            </v-list-item>
-          </v-list>
-
-          <div class="pa-2" style="text-align: center;">            
-            <v-btn inline-block @click.stop="logout">로그아웃</v-btn>
-          </div>
-        </v-navigation-drawer>
-
-        <v-app-bar
-          app
-          dense
-          color="#6A76AB"
-          dark
-          clipped-left
+        <v-btn
+          @click="loginModalToggle = !loginModalToggle"
+          v-if="!(isAuthenticated && isProfileLoaded)"
+          icon
         >
-          <v-toolbar-title style="margin: 0 auto; font-size: 40px;">TRIPLOG<v-icon>mdi-compass-outline</v-icon></v-toolbar-title>
+          <v-icon>mdi-account-arrow-right</v-icon>
+        </v-btn>
+      </v-app-bar>
+    </div>
 
-          <v-btn @click="loginModalToggle = !loginModalToggle"
-            v-if="!(isAuthenticated && isProfileLoaded)"
-            icon>
-            <v-icon>mdi-account-arrow-right</v-icon>
-          </v-btn>
-        </v-app-bar>
-      </div>
+    <v-main style="padding: 10px; margin:0 auto;">
+      <v-container>
+        <router-view></router-view>
+      </v-container>
+    </v-main>
 
-        <v-main style="padding: 10px; margin:0 auto;">
-          <v-container>
-            <router-view>
-            </router-view>
-          </v-container>
-        </v-main>
+    <v-footer fixed color="white">
+      <v-row justify="center" no-gutters>
+        <v-btn rounded color="deep-purple lighten-4" class="my-2" @click="goWrite">
+          <v-icon>mdi-file-edit</v-icon>
+          <div>WRITE</div>
+        </v-btn>
+      </v-row>
+    </v-footer>
 
-        <v-footer fixed color="white">
-          <v-row justify="center" no-gutters>
-            <v-btn rounded color="deep-purple lighten-4" class="my-2" @click="goWrite">
-              <v-icon>mdi-file-edit</v-icon>
-              <div>WRITE</div>
-            </v-btn>
-          </v-row>
-        </v-footer>
-    
+    <v-snackbar v-model="loginSuccess" timeout="5000">
+      <v-icon color="deep-orange darken-3">mdi-home</v-icon>
+      {{ getProfile + "님 반갑습니다." }}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="red" text v-bind="attrs" @click="loginSuccess = false">Close</v-btn>
+      </template>
+    </v-snackbar>
 
-    <v-snackbar
-        v-model="loginSuccess"
-        timeout="5000"
-      >
-        <v-icon color="deep-orange darken-3">mdi-home</v-icon>
-        {{ getProfile + "님 반갑습니다." }}
-        
-        <template v-slot:action="{ attrs }">
-          <v-btn
-            color="red"
-            text
-            v-bind="attrs"
-            @click="loginSuccess = false"
-          >
-            Close
-          </v-btn>
-        </template>
-      </v-snackbar>
+    <v-snackbar v-model="logoutSuccess" timeout="5000">
+      <v-icon color="deep-orange darken-3">mdi-home</v-icon>정상적으로 로그아웃 되었습니다.
+      <template v-slot:action="{ attrs }">
+        <v-btn color="red" text v-bind="attrs" @click="logoutSuccess = false">Close</v-btn>
+      </template>
+    </v-snackbar>
 
-      <v-snackbar
-        v-model="logoutSuccess"
-        timeout="5000"
-      >
-        <v-icon color="deep-orange darken-3">mdi-home</v-icon>
-         정상적으로 로그아웃 되었습니다.
-        
-        <template v-slot:action="{ attrs }">
-          <v-btn
-            color="red"
-            text
-            v-bind="attrs"
-            @click="logoutSuccess = false"
-          >
-            Close
-          </v-btn>
-        </template>
-      </v-snackbar>
+    <v-snackbar v-model="alert" timeout="5000">
+      <v-icon color="deep-orange darken-3">mdi-home</v-icon>
+      {{alertMsg}}
+      <template v-slot:action="{ attrs }">
+        <v-btn color="red" text v-bind="attrs" @click="alert = false">Close</v-btn>
+      </template>
+    </v-snackbar>
 
-      <v-snackbar
-        v-model="alert"
-        timeout="5000"
-      >
-        <v-icon color="deep-orange darken-3">mdi-home</v-icon>
-         {{alertMsg}}
-        
-        <template v-slot:action="{ attrs }">
-          <v-btn
-            color="red"
-            text
-            v-bind="attrs"
-            @click="alert = false"
-          >
-            Close
-          </v-btn>
-        </template>
-      </v-snackbar>
-
-        <v-dialog 
-          v-model="loginModalToggle"
-          max-width=800
-          persistent
-        >
-          <login v-on:closeLoginModal="closeLoginModal"></login>
-        </v-dialog>
-        <v-dialog 
-          v-model="userInfoModalToggle"
-          max-width=800
-        >
-          <user-info-comp 
-          v-if="userInfo.email"
-          :propImage="userInfo.imagesrc"
-          :propEmail="userInfo.email"
-          :propName="userInfo.name"
-          :propNickname="userInfo.nickname"
-          :propIntro="userInfo.intro"
-          :propValid="userInfo.valid"
-          :propJoinedAt="userInfo.joinedAt"
-          :key="userInfoCompKey"
-          v-on:closeUserInfoModal="closeUserInfoModal"
-          >
-          </user-info-comp>
-        </v-dialog>
+    <v-dialog v-model="loginModalToggle" max-width="800" persistent>
+      <login v-on:closeLoginModal="closeLoginModal"></login>
+    </v-dialog>
+    <v-dialog v-model="userInfoModalToggle" max-width="800">
+      <user-info-comp
+        v-if="userInfo.email"
+        :propImage="userInfo.imagesrc"
+        :propEmail="userInfo.email"
+        :propName="userInfo.name"
+        :propNickname="userInfo.nickname"
+        :propIntro="userInfo.intro"
+        :propValid="userInfo.valid"
+        :propJoinedAt="userInfo.joinedAt"
+        :key="userInfoCompKey"
+        v-on:closeUserInfoModal="closeUserInfoModal"
+      ></user-info-comp>
+    </v-dialog>
   </v-app>
 </template>
 
 <script>
-import { mapGetters, mapState } from 'vuex';
-import {AUTH_LOGOUT} from './store/actions/auth'
+import { mapGetters, mapState } from "vuex";
+import { AUTH_LOGOUT } from "./store/actions/auth";
 import Login from "@/components/account/Login.vue";
 import UserInfoComp from "@/components/account/UserInfoComp.vue";
 import http from "@/util/http-common";
-
 
 export default {
   name: "App",
@@ -207,114 +161,128 @@ export default {
     logoutSuccess: false,
     loginModalToggle: false,
     userInfoModalToggle: false,
-    userInfo:{},
+    userInfo: {},
     alert: false,
     alertMsg: "",
     userInfoCompKey: 0,
-    defaultSelected:"제목",
-    items: ["제목","작성자"],
+    defaultSelected: "제목",
+    items: ["제목", "작성자"],
     titleSearch: "",
     title: "UnTitled",
     mini: true,
     navitems: [
-      {title: 'Home', icon: 'mdi-home-city'},
-      {title: 'My Account', icon: 'mdi-account'},
-      {title: '좋아요 목록', icon: 'mdi-account-group-outline'},
-      {title: '게시물 목록', icon: 'mdi-account-group-outline'},
-    ],
+      { title: "Home", icon: "mdi-home-city" },
+      { title: "My Account", icon: "mdi-account" },
+      { title: "좋아요 목록", icon: "mdi-account-group-outline" },
+      { title: "게시물 목록", icon: "mdi-account-group-outline" }
+    ]
   }),
   components: {
     Login,
-    UserInfoComp,
+    UserInfoComp
   },
   methods: {
     goToMyBlog: function() {
       this.$router.push(`/${this.getUserNum}`);
     },
-    logout: function () {
+    logout: function() {
       this.$store.dispatch(AUTH_LOGOUT).then(() => {
         this.drawer = false;
-        const path = '/';
-        if (this.$route.path !== path) this.$router.push(path)
-      })
+        const path = "/";
+        if (this.$route.path !== path) this.$router.push(path);
+      });
     },
-    info: function () {
+    info: function() {
       http.get(`/users/get/${this.getUserNum}`).then(({ data }) => {
         this.userInfo = data;
         console.dir(data);
         this.userInfoCompKey += 1;
-        this.userInfoModalToggle=true;
+        this.userInfoModalToggle = true;
       });
     },
-    avatarName : function(name) {
+    avatarName: function(name) {
       var tempName = name.split(/(?=[A-Z])/);
-      if(tempName.length == 1) return tempName[0].substring(0, 2).toUpperCase();
-      else return (tempName[0].substring(0,1) + tempName[1].substring(0,1)).toUpperCase();
+      if (tempName.length == 1)
+        return tempName[0].substring(0, 2).toUpperCase();
+      else
+        return (
+          tempName[0].substring(0, 1) + tempName[1].substring(0, 1)
+        ).toUpperCase();
     },
-    randomColorGenerateor : function() {
-      var letters = '0123456789ABCDEF';
-      var color = '#';
+    randomColorGenerateor: function() {
+      var letters = "0123456789ABCDEF";
+      var color = "#";
       for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
       }
       this.ranColor = color;
     },
-    handleResize : function() {
+    handleResize: function() {
       this.clientHeight = document.documentElement.clientHeight;
     },
     closeLoginModal: function() {
       this.loginModalToggle = false;
-      this.$router.push('/');
+      this.$router.push("/");
     },
-    
+
     gitPage: function() {
-      window.open("https://lab.ssafy.com/edonc700/happyhousefinal", "PopupGit", "width=1000,height=900");
+      window.open(
+        "https://lab.ssafy.com/edonc700/happyhousefinal",
+        "PopupGit",
+        "width=1000,height=900"
+      );
     },
     goWrite: function() {
-      this.$router.push('/article/write');
+      this.$router.push("/article/write");
     },
     closeUserInfoModal: function(msg, afterNickName) {
-      if(msg != null) {
+      if (msg != null) {
         this.alertMsg = msg;
         this.alert = true;
-        this.$store.commit('modifyProfileName', afterNickName);
+        this.$store.commit("modifyProfileName", afterNickName);
       }
       this.userInfoModalToggle = false;
     },
     useSnackBar: function(msg) {
-      if(msg != null) {
+      if (msg != null) {
         this.alertMsg = msg;
         this.alert = true;
       }
     },
     notice: function() {
-      this.$router.push('noticeList');
+      this.$router.push("noticeList");
     }
   },
   computed: {
-    ...mapGetters(['isAuthenticated', 'isProfileLoaded','getProfile', 'getRealName', 'getEmail', 'getUserNum']),
+    ...mapGetters([
+      "isAuthenticated",
+      "isProfileLoaded",
+      "getProfile",
+      "getRealName",
+      "getEmail",
+      "getUserNum"
+    ]),
     ...mapState({
-      authLoading: state => state.auth.status === 'loading',
+      authLoading: state => state.auth.status === "loading",
       uname: state => `${state.user.getProfile}`,
-      userEmail : state => `${state.user.getEmail}`,
-      userNum : state => `${state.user.getUserNum}`
+      userEmail: state => `${state.user.getEmail}`,
+      userNum: state => `${state.user.getUserNum}`
     }),
-    loading: function () {
-      return this.authStatus === 'loading' && !this.isAuthenticated
+    loading: function() {
+      return this.authStatus === "loading" && !this.isAuthenticated;
     }
   },
   mounted() {
     this.randomColorGenerateor();
     this.handleResize();
-    window.addEventListener('resize', this.handleResize);
+    window.addEventListener("resize", this.handleResize);
   },
   watch: {
-    getProfile : function() {
-      if(this.getProfile == "") {
-        this.logoutSuccess= true;
-      }
-      else {
-        this.loginSuccess= true;
+    getProfile: function() {
+      if (this.getProfile == "") {
+        this.logoutSuccess = true;
+      } else {
+        this.loginSuccess = true;
       }
     }
   }
--- frontend/src/views/personal/MainPage.vue
+++ frontend/src/views/personal/MainPage.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
     <PersonalMainComp
-        :hostNum = "item.num"
-        :hostNickName = "item.nickname"
+      :hostNum="item.num"
+      :hostNickName="item.nickname"
+      :hostEmail="item.email"
+      :isMyBlog="isMyBlog"
     />
   </div>
 </template>
 
 <script>
+import { mapGetters, mapState } from "vuex";
 import PersonalMainComp from "@/components/personal/PersonalMainComp.vue";
 import http from "@/util/http-common";
 
@@ -19,15 +22,23 @@ export default {
   data: function() {
     return {
       item: {},
+      isMyBlog: false
     };
   },
   created() {
-    http
-      .get(`/users/get/${this.$route.params.hostNum}`)
-      .then(({ data }) => {
-        this.item = data;
-        console.dir(data);
-      });
+    http.get(`/users/get/${this.$route.params.hostNum}`).then(({ data }) => {
+      this.item = data;
+      console.dir(data);
+      if (this.getUserNum == this.item.num) {
+        this.isMyBlog = true;
+      }
+    });
   },
+  computed: {
+    ...mapGetters(["getUserNum"]),
+    ...mapState({
+      userNum: state => `${state.user.getUserNum}`
+    })
+  }
 };
 </script>
