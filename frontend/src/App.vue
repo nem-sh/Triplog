e9ac093183f627a964cf5838db0aaeeab21a0c38
정아영 <p_111@naver.com>
2020-08-06 17:42:20 +0900

    S03P13B207-10 complete

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -105,8 +105,8 @@
           <v-icon color="teal lighten-4">mdi-compass-outline</v-icon>
         </v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-form class="d-flex search align-items-center" action="/article/ArticleSearch">
-          <div class="d-flex">
+
+        <!-- <v-form action="/article/ArticleSearch">
           <v-text-field
               label="search"
               color="black"
@@ -122,15 +122,26 @@
               label-color="black"
               name="keyword"
             ></v-text-field>
-            
-            <v-icon class="d-flex" @click="searchtoggle = !searchtoggle"
-            align-center
-            ma-1
-            >
-            fas fa-search</v-icon> 
-          </div>
-          </v-form>
+        </v-form> -->
+        <v-expand-transition style="display: flex;">
+            <v-text-field 
+            label="Search"
+            background-color="cyan darken-1"
+            color="white"
+            outlined
+            v-show="searchtoggle"
+            class="mt-6 shrink"
+            rounded
+            dense
+            clearable
+          />
+        </v-expand-transition>
 
+        <v-btn x-large icon @click="searchtoggle = !searchtoggle">
+          <v-icon>
+          mdi-text-search</v-icon>
+        </v-btn>
+          
         <v-btn
           @click="loginModalToggle = !loginModalToggle"
           v-if="!(isAuthenticated && isProfileLoaded)"
@@ -368,8 +379,3 @@ export default {
   }
 };
 </script>
-<style>
-.text-black input {
-      color: black !important;
-    }
-</style>
