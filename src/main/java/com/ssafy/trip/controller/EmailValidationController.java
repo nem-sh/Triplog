6052648d4d98a04372b5092970452c6ce39418c2
정아영 <p_111@naver.com>
2020-08-12 17:43:17 +0900

    modify address

--- src/main/java/com/ssafy/trip/controller/EmailValidationController.java
+++ src/main/java/com/ssafy/trip/controller/EmailValidationController.java
@@ -29,7 +29,7 @@ public class EmailValidationController {
 
         user.setValid(true);
         userRepository.save(user);
-        return "redirect:http://localhost/emailauth/success";
+        return "redirect:http://i3b207.p.ssafy.io/emailauth/success";
     }
 	
 	@RequestMapping("/api/emailValid/re/{num}")
